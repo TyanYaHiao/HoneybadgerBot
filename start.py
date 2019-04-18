3020f4eba32994307488703ca0a0d3aa848d9496
tyanyahiao <tyanyahiao@gmail.com>
2019-04-18 19:48:54 +0300

    2

--- start.py
+++ start.py
@@ -1,11 +1,24 @@
 import telebot
+from secure import get_token
 
-bot = telebot.TeleBot("800429755:AAEYRVxoKGIs3_40PX8Ph_eS72jySUkCryQ")
+bot = telebot.TeleBot(get_token())
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['settings'])
+def choose_chat(message):
+    bot.reply_to(message, "Выберите чат, в который будут пересылаться сообщения")
+    bot.cha
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(commands=['hello'])
+def handle_start_help(message):
+    bot.reply_to(message, "Privet")
+    pass
 
 
 @bot.message_handler(func=lambda message: True)
