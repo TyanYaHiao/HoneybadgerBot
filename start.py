03e627b527ab44d4579439f3bec03641e25de530
tyanyahiao <tyanyahiao@gmail.com>
2020-11-23 18:32:02 +0300

    stocks add

--- start.py
+++ start.py
@@ -1,18 +1,16 @@
 import telebot
+import emoji
+import yfinance as yf
 from secure import get_token
 
 bot = telebot.TeleBot(get_token())
 
 
-@bot.message_handler(commands=['settings'])
-def choose_chat(message):
-    bot.reply_to(message, "Выберите чат, в который будут пересылаться сообщения")
-    bot.cha
-
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(message.chat.id, "Type ticker, get price and change\n"
+                                      "Try ticker + .me for moscow exhange\n"
+                                      "Use ticker + number to get days change")
 
 
 @bot.message_handler(commands=['hello'])
@@ -23,7 +21,30 @@ def handle_start_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    try:
+        message_words = str.split(message.text)
+        tick = yf.Ticker(message_words[0])
+        # bot.send_message(message.chat.id, "*bold *text*", parse_mode="Markdown")
+        name = str(tick.info['shortName'])
+        price = str(tick.info['open'])
+        currency = str(tick.info['currency'])
+        if len(message_words) > 1 and message_words[1].isdigit:
+            history = tick.history(period=str(message_words[1] + "d"))
+            change = round(((tick.info['open'] / history['Close'][0] - 1) * 100), 1)
+        else:
+            history = tick.history(period="20d")
+            change = round(((tick.info['open'] / history['Close'][0] - 1) * 100), 1)
+        if change >= 10:
+            change_sign = emoji.emojize("🚀")
+        elif change > 0:
+            change_sign = emoji.emojize("⬆")
+        elif change < 0:
+            change_sign = emoji.emojize("⬇")
+        else:
+            change_sign = ""
+        bot.send_message(message.chat.id, name + "\nPrice: " + price + " " + currency + ", " + str(change) + "% " + change_sign)
+    except:
+        bot.reply_to(message, "Error")
 
 
 bot.polling()
